203555c3ba3688a92938b75c0630016615bced66
Miguel Salinas Gancedo <Miguel.Salinas@dentsuaegis.com>
2021-01-13 00:21:14 +0100

    last update

--- GSCC_ZABBIX/Controllers/ZabbixApicoreController.cs
+++ GSCC_ZABBIX/Controllers/ZabbixApicoreController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.Xml;
+using System.Net;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,20 @@ namespace Zabbix.Controller
   [Route("api/zabbix-api-core")]
   public class ZabbixApiCoreController
   {
-      private ZabbixApiCoreService zabbixApiCoreService;
-  }
+      private IZabbixApiCoreService zabbixApiCoreService;
+
+      public ZabbixApiCoreController(IZabbixApiCoreService zabbixApiCoreService)
+      {
+            this.zabbixApiCoreService = zabbixApiCoreService;
+      }
+
+      [HttpGet("/get-json")]
+      [ProducesResponseType(typeof(Task<ZabbixAPICore.Response>), (int)HttpStatusCode.OK)]
+      public Task<ZabbixAPICore.Response> GetResponseJsonAsync(string method, object parameters)
+      {
+        Task<ZabbixAPICore.Response> response = this.zabbixApiCoreService.GetZabbix().GetResponseObjectAsync(method, parameters);
+
+        return response;
+      }
+    }
 }
